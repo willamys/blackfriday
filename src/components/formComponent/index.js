b66e878ae7c493923be49e3340c1cb4d6a659804
Willamys Araújo <willamysaraujo7@gmail.com>
2021-07-16 22:39:29 -0300

    Start component input form

---
+++ src/components/formComponent/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FormComponent() {
+  return (
+    <>
+      <input type="text" name="name" id="name" placeholder="Seu Nome" />
+      <input type="email" name="email" id="email" placeholder="Seu E-mail" />
+    </>
+  );
+}
+
+export default FormComponent;
